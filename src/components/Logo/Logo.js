19b557f0ac51d4fb76f0634b1dc30b3d8e6db6c0
preko96 <35433203+preko96@users.noreply.github.com>
2018-03-12 15:06:57 +0000

    BIG UPDATE

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import brain from './brain4.png'
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
-	return (
-		<div className="Logo ma4 mt0">
-			<img 
-				className="dim pointer grow"
-				alt='logo' src={brain} width="82" height="82"
-			/>
-		</div>
-	)
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+        </div>
+      </Tilt>
+    </div>
+  );
 }
 
 export default Logo;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App/App.js';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
